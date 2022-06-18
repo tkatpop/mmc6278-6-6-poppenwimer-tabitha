07c9633f9d5f6ce86b18ed2a1ca02f811ea8fe7d
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-06-18 21:00:20 +0300

    onclick function toggle method added

---
+++ index.js
@@ -0,0 +1,7 @@
+var hamburgerBtn = document.querySelector('.hamburger-btn')
+var hamburgerMenu = document.querySelector('.hamburger-menu')
+
+
+hamburgerBtn.onclick = function(){
+    hamburgerMenu.classList.toggle('show-menu')
+}
